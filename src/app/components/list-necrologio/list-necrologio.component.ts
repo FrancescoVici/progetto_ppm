951e40a204c3e24e7c985c9ba37c59a452fe7c53
FrancescoVici <francesco.vici@edu.unifi.it>
2024-05-08 14:47:02 +0200

    Aggiunto ListNecrologioComponent

---
+++ src/app/components/list-necrologio/list-necrologio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'list-necrologio',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './list-necrologio.component.html',
+  styleUrl: './list-necrologio.component.css',
+})
+export class ListNecrologioComponent {}

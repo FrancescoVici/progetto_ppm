d49d5d9fe7b57850fc590c3eadbc5e530fa6c1cc
FrancescoVici <francesco.vici@edu.unifi.it>
2024-05-10 10:41:03 +0200

    styling AppComponent e aggiunto CardLettiComponent

--- src/app/components/card-letti/card-letti.component.ts
+++ src/app/components/card-letti/card-letti.component.ts
@@ -4,12 +4,10 @@ import { Articolo } from '../../models';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'card-articolo',
+  selector: 'card-letti',
   standalone: true,
   imports: [RouterOutlet, CommonModule],
-  templateUrl: './card-articolo.component.html',
-  styleUrl: './card-articolo.component.css',
+  templateUrl: './card-letti.component.html',
+  styleUrl: './card-letti.component.css',
 })
-export class CardArticoloComponent {
-  @Input() public articolo: Articolo | null = null;
-}
+export class CardLettiComponent {}
--- src/app/components/card-widget/card-widget.component.ts
+++ src/app/components/card-widget/card-widget.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'card-widget',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './card-widget.component.html',
   styleUrl: './card-widget.component.css',
 })
-export class CardWidgetComponent {}
+export class CardWidgetComponent {
+  @Input() widgetName: string | null = null;
+  @Input() imagePath!: string;
+  @Input() title: string | null = null;
+  @Input() category: string | null = null;
+  @Input() author: string | null = null;
+}
